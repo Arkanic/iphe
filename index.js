d7f2fad0075e613953c08efed379ce24e2b9a68c
Arkanic <silverfishasaurus@gmail.com>
2020-01-09 20:42:00 +0000

    well that didn't work

--- index.js
+++ index.js
@@ -2,8 +2,27 @@ const http = require("http");
 const path = require("path");
 const fs = require("fs");
 const url = require("url");
-const handleRequest = require('./server/filehandle/handleRequest');
+const typeExt = require("./ext/types.json");
 
 const server = http.createServer(handleRequest);
 const port = process.env.PORT || 3000;
 server.listen(port);
+function handleRequest(request, response) {
+    let pathname = request.url;
+    if(pathname == "/") {
+        pathname = "/index.html";
+    }
+    pathname = "/client" + pathname;
+    let ext = path.extname(extname)
+    let contentType = typeExt[ext] || "text/plain";
+
+    fs.readFile(__dirname + pathname, function(error, data) {
+        if(error) {
+            response.writeHead(500);
+            return response.end("Error loading: " + pathname);
+        } else {
+            response.writeHead(200, {"Content-Type": contentType});
+            return response.end(data);
+        }
+    });
+};
