3f7e24201ea48d3a0544507aba5ebd4c59e61358
Arkanic <silverfishasaurus@gmail.com>
2020-01-10 18:33:27 +0000

    fix types (i don't know why i decided that would be a good idea)

--- server/ext/types.js
+++ server/ext/types.js
@@ -1,8 +1,8 @@
 module.exports = {
-    html = "text/html",
-    htm = "text/html",
-    css = "text/css",
-    js = "text/javascript",
-    png = "image/png",
-    jpg = "image/jpg"
+    html: "text/html",
+    htm: "text/html",
+    css: "text/css",
+    js: "text/javascript",
+    png: "image/png",
+    jpg: "image/jpg"
 };
