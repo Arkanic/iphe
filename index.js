e7e93707dafd7e140851f0bd8fc188071b9238e2
Arkanic <silverfishasaurus@gmail.com>
2020-01-12 18:23:29 +0000

    hhhhh

--- index.js
+++ index.js
@@ -4,32 +4,36 @@ const fs = require("fs");
 const url = require("url");
 
 const server = http.createServer(handleRequest);
-const port = process.env.PORT || 8000;
-server.listen(port);
-function handleRequest(request, response) {
-    let pathname = request.url;
-    if(pathname == "/") {
-        pathname = "/index.html";
-    }
-    pathname = "/client" + pathname;
-    let ext = path.extname(extname);
-    const typeExt = {
-        ".html": "text/html",
-        ".htm": "text/html",
-        ".css": "text/css",
-        ".js": "text/javascript",
-        ".png": "image/png",
-        ".jpg": "image/jpg",
-        ".json": "application/json"
-    };
-    let contentType = typeExt[ext] || "text/plain";
 
-    fs.readFile(__dirname + pathname, function(error, data) {
-        if(error) {
-            response.writeHead(500);
-            return response.end("Error loading: " + pathname);
-        }
-        response.writeHead(200, {"Content-Type": contentType});
-        response.end(data);
-    });
+const port = process.env.PORT || 3000;
+
+server.listen(port);
+
+function handleRequest(request, response) {
+  let pathname = request.url;
+
+  if(pathname == "/") {
+    pathname = "/client/play.html";
+  }
+
+  let ext = path.extname(pathname);
+
+  let typeExt = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".json": "application/json",
+    ".png": "image/png"
+  };
+
+  let contentType = typeExt[ext] || "text/plain";
+
+  fs.readFile(__dirname + pathname, function(error, data) {
+    if(error) {
+      response.writeHead(500);
+      return response.end("Error loading: " + pathname);
+    }
+    response.writeHead(200, {"Content-Type": contentType});
+    response.end(data);
+  });
 }
