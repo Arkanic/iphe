7db4581843bb327ba110c374eac33aa60f4ea302
Arkanic <silverfishasaurus@gmail.com>
2020-01-26 22:32:19 +0000

    more more fix

--- client/js/game.js
+++ client/js/game.js
@@ -1,9 +1,6 @@
 "use strict";
 
 function toggleUI(id) {
-    let idobject = document.getElementById(id).catch(() => {
-        return;
-    });
     if(document.getElementById(id).style.display == "none") {
         document.getElementById(id).style.display = "block";
     } else {
