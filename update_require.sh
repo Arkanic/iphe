5c031ce09c7adcbad62efb2b67380288252447ee
iphe-updater <silverfishasaurus@gmail.com>
2020-01-28 18:27:41 +0000

    make it manual

--- update_require.sh
+++ update_require.sh
@@ -1,5 +1,1 @@
-git config --global user.name "iphe-updater"
 git submodule add https://github.com/uladkasach/clientside-require clientside-require
-git add .
-git commit -m "Update to match ${TRAVIS_TAG}"
-git push https://Arkanic:${GH_PUSH_TOKEN}@github.com/Arkanic/iphe
