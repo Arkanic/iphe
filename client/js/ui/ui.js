a4da09f1d3d24c7516841ea050701b82a591092a
Arkanic <silverfishasaurus@gmail.com>
2020-01-31 20:22:26 +0000

    aaaaaaaaaa

--- client/js/ui/ui.js
+++
@@ -1,7 +0,0 @@
-export function toggleUI(id) {
-    if(document.getElementById(id).style.display == "none") {
-        document.getElementById(id).style.display = "block";
-    } else {
-        document.getElementById(id).style.display = "none";
-    }
-}
